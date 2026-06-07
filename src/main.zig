1ba2926fa0efa3f39f039f0d23491a23502978a8
Alexander Taylor <alexander@inclem.net>
2026-06-07 22:56:06 +0100

    Test reading from file

--- src/main.zig
+++ src/main.zig
@@ -25,11 +25,10 @@ const example_fragment =
     \\RU[Japanese]
     \\GN[友谊赛]
     \\;B[pd]
-    // \\(;W[dp]
-    // \\C[GeRui: hi
-    // \\]
-    // \\))
-    \\)
+    \\(;W[dp]
+    \\C[GeRui: hi
+    \\]
+    \\))
 ;
 
 const example_sgf =
@@ -326,6 +325,354 @@ const example_sgf =
     \\))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))
 ;
 
+const example_sgf_2 =
+    \\(;GM[1]FF[4]CA[UTF-8]AP[GoPanda2.5.2-3]ST[1]
+    \\SZ[19]HA[0]KM[0.5]
+    \\PW[Andrius]WR[4d]PB[blit]BR[3d+]RE[W+17.5]DT[2020-01-21]PC[IGS-PandaNet]
+    \\
+    \\;B[pd]
+    \\;W[pp]
+    \\;B[cd]
+    \\;W[dp]
+    \\;B[fd]
+    \\;W[cj]
+    \\;B[fq]
+    \\;W[dn]
+    \\;B[qq]
+    \\;W[pq]
+    \\;B[qp]
+    \\;W[po]
+    \\;B[qo]
+    \\;W[qn]
+    \\;B[rn]
+    \\;W[jp]
+    \\;B[dq]
+    \\;W[cq]
+    \\;B[cr]
+    \\;W[eq]
+    \\;B[dr]
+    \\;W[ep]
+    \\;B[er]
+    \\;W[bq]
+    \\;B[hq]
+    \\;W[jr]
+    \\;B[qm]
+    \\;W[pn]
+    \\;B[qr]
+    \\;W[nc]
+    \\;B[qf]
+    \\;W[hc]
+    \\;B[fc]
+    \\;W[jd]
+    \\;B[pm]
+    \\;W[om]
+    \\;B[ol]
+    \\;W[pl]
+    \\;B[ql]
+    \\;W[pk]
+    \\;B[ok]
+    \\;W[pj]
+    \\;B[oj]
+    \\;W[pi]
+    \\;B[oi]
+    \\;W[pg]
+    \\;B[oh]
+    \\;W[qg]
+    \\;B[on]
+    \\;W[nm]
+    \\;B[nn]
+    \\;W[mm]
+    \\;B[mn]
+    \\;W[ln]
+    \\;B[lm]
+    \\;W[ml]
+    \\;B[ll]
+    \\;W[mk]
+    \\;B[mp]
+    \\;W[mr]
+    \\;B[lr]
+    \\;W[mq]
+    \\;B[ko]
+    \\;W[lq]
+    \\;B[kn]
+    \\;W[lk]
+    \\;B[lh]
+    \\;W[jk]
+    \\;B[pf]
+    \\;W[og]
+    \\;B[kj]
+    \\;W[kk]
+    \\;B[jm]
+    \\;W[pb]
+    \\;B[nd]
+    \\;W[mg]
+    \\;B[mh]
+    \\;W[qc]
+    \\;B[mc]
+    \\;W[oc]
+    \\;B[lg]
+    \\;W[mf]
+    \\;B[le]
+    \\;W[lf]
+    \\;B[kf]
+    \\;W[me]
+    \\;B[ke]
+    \\;W[md]
+    \\;B[kc]
+    \\;W[lc]
+    \\;B[jc]
+    \\;W[kd]
+    \\;B[ld]
+    \\;W[mb]
+    \\;B[lb]
+    \\;W[mc]
+    \\;B[ic]
+    \\;W[id]
+    \\;B[hd]
+    \\;W[jf]
+    \\;B[jg]
+    \\;W[if]
+    \\;B[ig]
+    \\;W[hf]
+    \\;B[gd]
+    \\;W[hb]
+    \\;B[ib]
+    \\;W[kb]
+    \\;B[il]
+    \\;W[ik]
+    \\;B[hk]
+    \\;W[lo]
+    \\;B[lp]
+    \\;W[hj]
+    \\;B[ij]
+    \\;W[gk]
+    \\;B[hl]
+    \\;W[gj]
+    \\;B[hg]
+    \\;W[gf]
+    \\;B[jj]
+    \\;W[cf]
+    \\;B[fm]
+    \\;W[el]
+    \\;B[pr]
+    \\;W[or]
+    \\;B[nq]
+    \\;W[np]
+    \\;B[op]
+    \\;W[oq]
+    \\;B[no]
+    \\;W[nr]
+    \\;B[ir]
+    \\;W[iq]
+    \\;B[hr]
+    \\;W[gg]
+    \\;B[kp]
+    \\;W[ls]
+    \\;B[kq]
+    \\;W[kr]
+    \\;B[rj]
+    \\;W[ri]
+    \\;B[qj]
+    \\;W[qi]
+    \\;B[be]
+    \\;W[bf]
+    \\;B[gh]
+    \\;W[fh]
+    \\;B[hh]
+    \\;W[dd]
+    \\;B[dc]
+    \\;W[de]
+    \\;B[gb]
+    \\;W[ia]
+    \\;B[fl]
+    \\;W[ek]
+    \\;B[br]
+    \\;W[bc]
+    \\;B[cc]
+    \\;W[bd]
+    \\;B[ce]
+    \\;W[ae]
+    \\;B[bb]
+    \\;W[ab]
+    \\;B[ad]
+    \\;W[ac]
+    \\;B[ba]
+    \\;W[cb]
+    \\;B[db]
+    \\;W[ef]
+    \\;B[aq]
+    \\;W[bp]
+    \\;B[ap]
+    \\;W[ao]
+    \\;B[ar]
+    \\;W[bo]
+    \\;B[em]
+    \\;W[dm]
+    \\;B[jq]
+    \\;W[go]
+    \\;B[gp]
+    \\;W[fo]
+    \\;B[ho]
+    \\;W[hn]
+    \\;B[in]
+    \\;W[gn]
+    \\;B[gl]
+    \\;W[qk]
+    \\;B[rk]
+    \\;W[sj]
+    \\;B[rl]
+    \\;W[gi]
+    \\;B[fk]
+    \\;W[fj]
+    \\;B[sk]
+    \\;W[si]
+    \\;B[ph]
+    \\;W[qh]
+    \\;B[ng]
+    \\;W[nf]
+    \\;B[nh]
+    \\;W[of]
+    \\;B[fe]
+    \\;W[ff]
+    \\;B[hi]
+    \\;W[en]
+    \\;B[np]
+    \\;W[ps]
+    \\;B[qs]
+    \\;W[os]
+    \\;B[ee]
+    \\;W[hm]
+    \\;B[im]
+    \\;W[df]
+    \\;B[ed]
+    \\;W[ga]
+    \\;B[fa]
+    \\;W[jb]
+    \\;B[ca]
+    \\;W[af]
+    \\;B[ha]
+    \\;W[je]
+    \\;B[kg]
+    \\;W[ga]
+    \\;B[js]
+    \\;W[gc]
+    \\;B[ha]
+    \\;W[sm]
+    \\;B[rm]
+    \\;W[ga]
+    \\;B[fb]
+    \\;W[ks]
+    \\;B[is]
+    \\;W[he]
+    \\;B[ha]
+    \\;W[io]
+    \\;B[hp]
+    \\;W[ga]
+    \\;B[fp]
+    \\;W[fn]
+    \\;B[ha]
+    \\;W[ro]
+    \\;B[rp]
+    \\;W[ga]
+    \\;B[aa]
+    \\;W[ad]
+    \\;B[oo]
+    \\;W[ha]
+    \\;B[ge]
+    \\)
+    \\
+;
+
+const example_sgf_3 =
+    \\(;GM[1]FF[4]CA[UTF-8]AP[CGoban:3]ST[2]
+    \\RU[AGA]SZ[19]KM[7.50]
+    \\PW[White]PB[Black]
+    \\;B[pd]
+    \\;W[qc]
+    \\;B[pc]
+    \\;W[qd]
+    \\(;B[qe]
+    \\;W[re]
+    \\;B[qf]
+    \\;W[rf]
+    \\;B[qg]
+    \\(;W[pb]
+    \\;B[ob]
+    \\;W[qb]
+    \\;B[nc])
+    \\(;W[rg]
+    \\;B[qh]
+    \\;W[dp]
+    \\;B[pb]
+    \\;W[dd]
+    \\;B[rb]
+    \\;W[rh]
+    \\;B[ri]
+    \\;W[rc]
+    \\(;B[sc]
+    \\;W[sd]
+    \\;B[sb]
+    \\;W[qb]
+    \\;B[qa]
+    \\;W[rd]
+    \\;B[ra]
+    \\;W[rj])
+    \\(;B[sd]
+    \\;W[sc]
+    \\;B[sh]
+    \\;W[qb]
+    \\;B[qa]
+    \\;W[qi]
+    \\;B[rj]
+    \\;W[pi]
+    \\;B[qk])))
+    \\(;B[qf]
+    \\(;W[rf]
+    \\(;B[rg]
+    \\;W[re]
+    \\(;B[qg]
+    \\;W[pb]
+    \\;B[ob]
+    \\;W[qb]
+    \\;B[nc])
+    \\(;B[pg]
+    \\;W[pe]
+    \\;B[qe]
+    \\;W[pb]
+    \\;B[ob]
+    \\;W[qb]
+    \\;B[nc]))
+    \\(;B[pe]
+    \\;W[rg]))
+    \\(;W[qe]
+    \\;B[pe]
+    \\;W[rf]
+    \\;B[qb]
+    \\;W[rb]
+    \\(;B[rg]
+    \\;W[pb]
+    \\;B[pf]
+    \\;W[ob])
+    \\(;B[re]
+    \\(;W[rd]
+    \\;B[rg]
+    \\;W[se]
+    \\;B[qg])
+    \\(;W[pb]
+    \\;B[rc]
+    \\;W[qa]
+    \\;B[rd]
+    \\;W[qb]
+    \\;B[rg]
+    \\;W[pf]
+    \\;B[qg]
+    \\;W[nc]
+    \\;B[nf]
+    \\;W[ld]))))
+    \\(;B[pe]
+    \\;W[rf]))
+;
+
 const Rgb = struct {
     r: u8,
     g: u8,
@@ -448,9 +795,36 @@ fn recursiveParseGameTree() mecha.Parser(rawGameTreeStruct) {
     return parseGameTree;
 }
 
+fn debugPrintNSpaces(num: u32) void {
+    for (0..num) |_| {
+        std.debug.print(" ", .{});
+    }
+}
+
 const rawGameTreeStruct = struct {
     sequence: rawSequenceStruct,
     sub_game_trees: []rawGameTreeStruct,
+
+    fn pretty_print(self: rawGameTreeStruct, extras: struct { depth: u32 = 0 }) void {
+        debugPrintNSpaces(extras.depth * 2);
+        std.debug.print("rawGameTreeStruct:\n", .{});
+        for (self.sequence.nodes) |node| {
+            debugPrintNSpaces(extras.depth * 2);
+            std.debug.print("  Node:\n", .{});
+            for (node.properties) |property| {
+                debugPrintNSpaces(extras.depth * 2);
+                std.debug.print("    {s} =", .{property.ident});
+                for (property.values) |value| {
+                    std.debug.print(" \"{s}\"", .{value});
+                }
+                std.debug.print("\n", .{});
+            }
+        }
+
+        for (self.sub_game_trees) |sub_game_tree| {
+            sub_game_tree.pretty_print(.{ .depth = extras.depth + 1 });
+        }
+    }
 };
 fn parsedGameTreeToStruct(result: struct { rawSequenceStruct, []rawGameTreeStruct }) rawGameTreeStruct {
     return .{
@@ -463,36 +837,18 @@ pub fn main(init: std.process.Init) !void {
     // This is appropriate for anything that lives as long as the process.
     const allocator: std.mem.Allocator = init.arena.allocator();
 
-    // const a = (try rgb.parse(allocator, "#aabbcc")).value.ok;
-    // std.debug.print("{any}\n", .{a});
+    const io = init.io;
+    const contents = try std.Io.Dir.readFileAlloc(
+        try std.Io.Dir.cwd().openDir(io, "example-sgfs", .{}),
+        io,
+        "3-3_invasion_variations.sgf",
+        allocator,
+        .unlimited,
+    );
+    defer allocator.free(contents);
 
-    const parsedSgf = (try parseGameTree.parse(allocator, example_fragment)).value.ok;
-    std.debug.print("{any}\n", .{parsedSgf});
-
-    for (parsedSgf.sequence.nodes) |node| {
-        std.debug.print("Node:\n", .{});
-        for (node.properties) |property| {
-            std.debug.print("  {s} =", .{property.ident});
-            for (property.values) |value| {
-                std.debug.print(" \"{s}\"", .{value});
-            }
-            std.debug.print("\n", .{});
-        }
-    }
-
-    // inline for (parsedSgf) |item| {
-    //     std.debug.print("{any}\n", .{item});
-    // }
-
-    // const parsedSgf = (try parseGameTree.parse(allocator, example_fragment));
+    const parsedSgf = (try parseGameTree.parse(allocator, contents)).value.ok;
     // std.debug.print("{any}\n", .{parsedSgf});
 
-    // const b = (try rgb.parse(allocator, "#abc")).value.ok;
-    // std.debug.print("{} {} {}\n", .{ b.r, b.g, b.b });
-
-    // const c = (try rgb.parse(allocator, "#000000")).value.ok;
-    // std.debug.print("{} {} {}\n", .{ c.r, c.g, c.b });
-
-    // const d = (try rgb.parse(allocator, "#000")).value.ok;
-    // std.debug.print("{} {} {}\n", .{ d.r, d.g, d.b });
+    parsedSgf.pretty_print(.{});
 }
